92c67f4437d5507ed8e8e0b1efde2107020b5194
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-10-21 09:31:19 +0200

    ajout de la connexion du rédacteur au site

--- connection_bdd.php
+++ connection_bdd.php
@@ -1,7 +1,7 @@
 <?php
 function connection(){
   try {
-    return $objPdo = new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=ProjetPhp','journet9u_appli ','23JAN2001'); //a compléter avec dans l'ordre: nom de la base de données, identifiant utilisateur de la bdd, mot de passe (numéro étudiant)
+    return $objPdo = new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=journet9u_ProjetPhp','journet9u_appli ','23JAN2001'); //a compléter avec dans l'ordre: nom de la base de données, identifiant utilisateur de la bdd, mot de passe (numéro étudiant)
   }
   catch(Exception $exception){
     session_start();
@@ -15,4 +15,3 @@ function connection(){
     exit();
   }
 }
-?>
--- connection_redacteur.php
+++ connection_redacteur.php
@@ -1,14 +1,55 @@
 <!-- page de connection d'un rédacteur au site pour pouvoir créer et intéragir avec les fonctionnalitées du site -->
-<?php 
 
+<?php
+session_start();
+if (isset($_SESSION['pseudo'])) {
+    $host = $_SERVER['HTTP_HOST'];
+    $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = 'page_accueil.php';
+    header("Location: http://$host$uri/$extra");
+}
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Connection au site</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+    <title>Connection au site</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">
+                        <!-- ajouter le lien vers la page correspondante -->
+                        <div>Inscription</div>
+                    </a></li>
+                <li><a href="./page_accueil.php">
+                        <!-- ajouter le lien vers la page correspondante -->
+                        <div>Retour à l'accueil</div>
+                    </a></li>
+            </ul>
+        </nav>
+    </header>
+    <article>
+        <h1>Connexion</h1>
+        <form method="POST" action=""> <br> <!-- ajouter le lien vers la page gérant la connexion du rédacteur -->
+            <div>
+                Identifiant :
+                <input type="text" size="20" placeholder="pseudo ou email" name="identifiant" required autocomplete="off" /> <br> <br>
+
+                Mot de passe :
+                <input type="password" size="20" placeholder="password" name="motdepasse" required />
+                <?php
+                if (isset($_SESSION['pb'])) echo "cet identifiant n'existe pas ou le mot de passe est erroné";
+                unset($_SESSION["pb"]);
+                ?>
+            </div>
+            <input type="submit" name="connexion" value="connction" />
+        </form>
+    </article>
 </body>
+
 </html>
