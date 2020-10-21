e4d00dcc1506bc4290839571c13e2de1fad8cbde
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-10-21 09:00:10 +0200

    ajout connection rédac

---
+++ connection_redacteur.php
@@ -0,0 +1,14 @@
+<!-- page de connection d'un rédacteur au site pour pouvoir créer et intéragir avec les fonctionnalitées du site -->
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Connection au site</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+</body>
+</html>
