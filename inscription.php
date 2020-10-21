b3d7c0654847b6f81e07ee3a3b92ddd138e64442
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-10-21 10:35:26 +0200

    ajout inscritpion au site et modification des liens dans accueil pour accéder aux autres pages

--- inscription.php
+++ inscription.php
@@ -30,20 +30,42 @@ if (isset($_SESSION['pseudo'])) {
         </nav>
     </header>
     <article>
-        <h1>Connexion</h1>
+        <h1>Inscription</h1>
         <form method="POST" action=""> <br> <!-- ajouter le lien vers la page gérant la connexion du rédacteur -->
             <div>
 
-                Nom:
-                <input type="text" size="60" placeholder="nom" name="nom" required autocomplete="off" />
+                Nom :
+                <input type="text" size="60" placeholder="nom" name="nom" <?php
+                                                                            if (isset($_SESSION['nom'])) echo 'value=" ' . $_SESSION['nom'] . '"';
+                                                                            unset($_SESSION['nom']);
+                                                                            ?> required autocomplete="off" /> <br> <br>
 
-
+                Prenom :
+                <input type="text" size="50" placeholder="prenom" name="prenom" required <?php
+                                                                                            if (isset($_SESSION['prenom'])) echo 'value="' . $_SESSION['prenom'] . '"'
+                                                                                            ?> autocomplete="off" /> <br> <br>
 
                 Email:
-                <input type="text" size="320" placeholder="mèl" name="email" required /> <br> <br>
+                <input type="text" size="50" placeholder="mèl" name="email" <?php
+                                                                            if (isset($_SESSION['mail'])) echo 'value="' . $_SESSION['mail'] . '"';
+                                                                            unset($_SESSION['mail']);
+                                                                            ?> required autocomplete="off" /> </br> </br>
+                <?php if (isset($_SESSION['pb']) && ($_SESSION['pb'] == 1 || $_SESSION['pb'] == 3)) echo "ce mail existe déjà</br>"; ?>
 
                 Mot de passe :
-                <input type="password" size="20" placeholder="password" name="motdepasse" required />
+                <input type="password" size="30" placeholder="password" name="motdepasse" <?php
+                                                                                            if (isset($_SESSION['mdp'])) echo 'value="' . $_SESSION['mdp'] . '"';
+                                                                                            unset($_SESSION['mdp']);
+                                                                                            ?> required /> </br> </br>
+
+                Vérification du mot de passe :
+                <input id="mdp2" type="password" oninput="check_mdp()" size="30" placeholder="ressaisissez le mot de passe" name="mdp" <?php
+                                                                                                                                        if (isset($_SESSION['mdp'])) echo 'value="' . $_SESSION['mdp'] . '"';
+                                                                                                                                        unset($_SESSION['mdp']);
+                                                                                                                                        ?> required /> </br> </br>
+                <p id="label_mdp">
+
+                </p>
 
 
                 Pseudo :
@@ -52,12 +74,12 @@ if (isset($_SESSION['pseudo'])) {
                                                                                                                 unset($_SESSION['pseudo²']);
                                                                                                                 ?> required autocomplete="off" /> </br> </br>
 
-                <?php
-                if (isset($_SESSION['pb'])) echo "cet identifiant n'existe pas ou le mot de passe est erroné";
-                unset($_SESSION["pb"]);
-                ?>
+                <?php if (isset($_SESSION['pb']) && ($_SESSION['pb'] == 1 || $_SESSION['pb'] == 2)) echo "ce pseudo existe déjà</br>"; ?>
+
+                <?php unset($_SESSION['pb']); ?>
+                </br>
             </div>
-            <input type="submit" name="connexion" value="connction" />
+            <input type="submit" onclick="javascript: return check_mdp_valider();" name="s'inscrire" value="inscription" />
         </form>
     </article>
 </body>
