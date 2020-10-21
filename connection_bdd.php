562398e44bd577bb3098f5d32511631eeb188785
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-10-21 08:49:41 +0200

    Ajout connection et erreur bdd

---
+++ connection_bdd.php
@@ -0,0 +1,18 @@
+<?php
+function connection(){
+  try {
+    return $objPdo = new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=','utilisateur ','mdp'); //a compléter avec dans l'ordre: nom de la base de données, identifiant utilisateur de la bdd, mot de passe (numéro étudiant)
+  }
+  catch(Exception $exception){
+    session_start();
+    $_SESSION['BDD_ERROR']=$exception->getMessage();
+
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = 'erreur_bdd.php';
+
+    header("Location: http://$host$uri/$extra");
+    exit();
+  }
+}
+?>
---
+++ erreur_bdd.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+if(!isset($_SESSION['BDD_ERROR'])){ // regarde si la personne qui accède à la page est déjà connecté, si elle l'est, elle se fait rediriger à la page d'accueil
+  $host  = $_SERVER['HTTP_HOST'];
+  $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+  $extra = '.php'; //nom de la page d'accueil à ajouter pour la redirection
+  header("Location: http://$host$uri/$extra");
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Erreur dans la base de données</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="./page_acceuil.php"><div>retour à l'acceuil</div></a></li>
+        </ul>
+      </nav>
+    </header>
+    <article>
+      <h1> Erreur au niveau de la base de données</h1>
+      <p>
+        <?php
+        echo $_SESSION['BDD_ERROR'];
+        unset($_SESSION['BDD_ERROR']);
+         ?>
+        </br>
+        </br>
+        Veillez contacter un administrateur
+      </p>
+    </article>
+
+  </body>
+</html>
