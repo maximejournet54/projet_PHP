dee84f127c6b4425550280c9bc260cb9e4db4c1b
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2020-10-21 11:58:37 +0200

    Erreurs dans la page ajoutArticle

--- ajoutArticle.php
+++ ajoutArticle.php
@@ -1,4 +1,6 @@
 <?php
+include ('connection_bdd.php');
+$result = $objPdo->query('SELECT description FROM theme'); 
     if(isset($_POST['ajouter'])){
         $erreur=[];
         $value=[];
@@ -9,21 +11,22 @@
             $value['titre']=trim($_POST['titre']);
         }
         if (!isset($_POST['texte']) or strlen(trim($_POST['texte']))==0) {
-            $erreur['texte']='saisie obligatoire du texte de l''article';
+            $erreur['texte']='saisie obligatoire du texte de l\'article';
         }
         else {
             $value['texte']=trim($_POST['texte']);
         }
 
         if (count($erreur)==0) {
-            include "connection_bdd.php";
+            
             $req="INSERT INTO news(idtheme,titrenews,textenews);
             VALUES (:idtheme,:titrenews:textenews)";
             $insert=$objPdo->prepare($req);
             $insert->execute(array(':idtheme'=>$_POST['theme'],':titrenews'=>$_POST['titre'],
             ':textenews'=>$_POST['texte']));
 
-            $result = $objPdo1->query('select description from theme,news where theme.idtheme=news.idtheme'); 
+            
+            
 
         }
 
@@ -43,21 +46,27 @@
         <title>Ajout d'un article</title>
     </head>
     <body>
+        <?php 
+        var_dump($result[0]);
+            exit();?>
         <center>
             <h1>Ajout d'un article</h1>
             <form method="POST" name="ajoutArticle"></form>
             <label for="theme">theme : </label><input type="radio" name="theme" placeholder="insérer le thème" 
             value="<?php
             foreach ($result as $row) {
-                echo .$row['description']];
+                echo  '"'. $row['description'] ;
+                var_dump($row['description']);
             }
             ?>">
 
             <br/>
             <br>
-            <label for="titre">titre : </label><input type="text" name="titre" placeholder="insérer le titre" value="<?php echo $value['titre']?>">
+            <label for="titre">titre : </label><input type="text" name="titre" placeholder="insérer le titre" value="<?php echo $value['titre']?>"/>
             <br>
-            <span class="erreur"><?php echo $erreur['titre']?></span> 
+            <span class="erreur"><?php if (isset($erreur['titre'])) {
+                echo $erreur['titre'];
+            } ?></span> 
             <br>
             <label for="texte">texte : </label> <textarea name="texte" placeholder="insérer le texte" maxlength=500 value="<?php echo $value['texte']?>"></textarea>
             <br>
@@ -69,3 +78,4 @@
     </body>
 </html>
 
+            
