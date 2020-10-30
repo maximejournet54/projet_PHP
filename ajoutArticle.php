06dce86135af2069c921e35c0011167e5f971514
maximejournet54 <maxime.journet@free.fr>
2020-10-30 13:38:43 +0100

    fixed error in ajoutArticles because of connection_bdd

--- ajoutArticle.php
+++ ajoutArticle.php
@@ -1,78 +1,76 @@
 <?php
-include ('connection_bdd.php');
-$result = $objPdo->query('SELECT description FROM theme'); 
-    if(isset($_POST['ajouter'])){
-        $erreur=[];
-        $value=[];
-        if (!isset($_POST['titre']) or strlen(trim($_POST['titre']))==0) {
-            $erreur['titre']='saisie obligatoire du titre';
-        }
-        else {
-            $value['titre']=trim($_POST['titre']);
-        }
-        if (!isset($_POST['texte']) or strlen(trim($_POST['texte']))==0) {
-            $erreur['texte']='saisie obligatoire du texte de l\'article';
-        }
-        else {
-            $value['texte']=trim($_POST['texte']);
-        }
+    include "connection_bdd.php";
+    error_reporting(E_ALL & ~E_NOTICE);
+    $req='SELECT description from THEME';
+    $result=$objPdo->prepare($req);
+    $result->execute();
+   
+        if(isset($_POST['ajouter'])){
+            $erreur=[];
+            $value=[];
+            if (!isset($_POST['titre']) or strlen(trim($_POST['titre']))==0) {
+                $erreur['titre']='saisie obligatoire du titre';
+            }
+            else {
+                $value['titre']=trim($_POST['titre']);
+            }
+            if (!isset($_POST['texte']) or strlen(trim($_POST['texte']))==0) {
+                $erreur['texte']='saisie obligatoire du texte de l\'article';
+            }
+            else {
+                $value['texte']=trim($_POST['texte']);
+            }
 
-        if (count($erreur)==0) {
-            
-            $req="INSERT INTO news(idtheme,titrenews,textenews);
-            VALUES (:idtheme,:titrenews:textenews)";
-            $insert=$objPdo->prepare($req);
-            $insert->execute(array(':idtheme'=>$_POST['theme'],':titrenews'=>$_POST['titre'],
-            ':textenews'=>$_POST['texte']));
+            if (count($erreur)==0) {
+                include "connection_bdd.php";
+                $req="INSERT INTO news(idtheme,titrenews,textenews) VALUES (:idtheme,:titrenews:textenews)";
+                $insert=$objPdo->prepare($req);
+                $insert->execute(array(':idtheme'=>$_POST['theme'],':titrenews'=>$_POST['titre'],
+                ':textenews'=>$_POST['texte']));
+            }
 
-            
-            
-
+            try {
+                $insert->execute();
+                unset($insert);
+            } catch (Exception $exception) {
+                die($exception->getMessage());
+            }
         }
-
-        try {
-            $insert->execute();
-            unset($insert);
-        } catch (Exception $exception) {
-            die($exception->getMessage());
-        }
-    }
 ?>
 
-<!DOCTYPE html>
+<html>
     <html lang="fr">
     <head>
         <meta charset="UTF-8">
         <title>Ajout d'un article</title>
     </head>
     <body>
-        <?php 
-        var_dump($result[0]);
-            exit();?>
+        
         <center>
             <h1>Ajout d'un article</h1>
-            <form method="POST" name="ajoutArticle"></form>
-            <label for="theme">theme : </label><input type="radio" name="theme" placeholder="insérer le thème" 
-            value="<?php
-            foreach ($result as $row) {
-                echo  '"'. $row['description'] ;
-                var_dump($row['description']);
-            }
-            ?>">
+            <form method="POST" name="ajoutArticle">
+                <label for="theme">theme : </label>
+                <input type="radio" name="theme" placeholder="insérer le thème" 
+                value="<?php
+                    foreach ($result as $row) {
+                        echo  $row;
+                    }
+                ?>">
 
-            <br/>
-            <br>
-            <label for="titre">titre : </label><input type="text" name="titre" placeholder="insérer le titre" value="<?php echo $value['titre']?>"/>
-            <br>
-            <span class="erreur"><?php if (isset($erreur['titre'])) {
-                echo $erreur['titre'];
-            } ?></span> 
-            <br>
-            <label for="texte">texte : </label> <textarea name="texte" placeholder="insérer le texte" maxlength=500 value="<?php echo $value['texte']?>"></textarea>
-            <br>
-            <span class="erreur"><?php echo $erreur['texte']?></span> 
-            <br>
-            <input type="submit" name="valider" value="valider">
+                <br/>
+                <br>
+                <label for="titre">titre : </label><input type="text" name="titre" placeholder="insérer le titre" value="<?php echo $value['titre']?>"/>
+                <br>
+                <span class="erreur"><?php if (isset($erreur['titre'])) {
+                    echo $erreur['titre'];
+                } ?></span> 
+                <br>
+                <label for="texte">texte : </label> <textarea name="texte" placeholder="insérer le texte" maxlength=500 value="<?php echo $value['texte']?>"></textarea>
+                <br>
+                <span class="erreur"><?php echo $erreur['texte']?></span> 
+                <br>
+                <input type="submit" name="valider" value="valider">
+            </form>
         </center>
 
     </body>
--- connection_bdd.php
+++ connection_bdd.php
@@ -1,17 +1,41 @@
 <?php
-function connection(){
-  try {
-    return $objPdo = new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=journet9u_ProjetPhp','journet9u_appli ','23JAN2001'); //a compléter avec dans l'ordre: nom de la base de données, identifiant utilisateur de la bdd, mot de passe (numéro étudiant)
-  }
-  catch(Exception $exception){
-    session_start();
-    $_SESSION['BDD_ERROR']=$exception->getMessage();
+/*
+  function connection(){
+    try {
+      return $objPdo = new PDO('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306;dbname=journet9u_ProjetPhp','journet9u_appli ','23JAN2001'); //a compléter avec dans l'ordre: nom de la base de données, identifiant utilisateur de la bdd, mot de passe (numéro étudiant)
+    }
+    catch(Exception $exception){
+      session_start();
+      $_SESSION['BDD_ERROR']=$exception->getMessage();
 
-    $host  = $_SERVER['HTTP_HOST'];
-    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-    $extra = 'erreur_bdd.php';
+      $host  = $_SERVER['HTTP_HOST'];
+      $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+      $extra = 'erreur_bdd.php';
 
-    header("Location: http://$host$uri/$extra");
-    exit();
+      header("Location: http://$host$uri/$extra");
+      exit();
+    }
   }
-}
+*/
+
+//comme dans les td et ça marche
+$db_config=array();
+$db_config['SGBD']='mysql';
+$db_config['HOST']='devbdd.iutmetz.univ-lorraine.fr';
+$db_config['DB_NAME']='journet9u_ProjetPhp';
+$db_config['USER']='journet9u_appli';
+$db_config['PASSWORD']='23JAN2001';
+
+
+//======================================================
+//connection avec PDO
+    try{$objPdo = new PDO
+        ($db_config['SGBD'].':host='.$db_config['HOST'].';dbname='.$db_config['DB_NAME'],
+        $db_config['USER'], $db_config['PASSWORD']);
+        //echo("ça marche!");
+        unset($db_config);
+    }catch(Exception $exception){
+        die($exception->getMessage());
+    }
+
+?>
