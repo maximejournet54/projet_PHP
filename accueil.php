2586909549e6140e7f29b522d80970d45ff98291
maximejournet54 <maxime.journet@free.fr>
2020-10-31 17:25:11 +0100

    css accueil+ fix datalist theme in ajoutArticle

---
+++ accueil.php
@@ -0,0 +1,53 @@
+<?php
+    include "connection_bdd.php";
+    $result = $objPdo->query('SELECT titrenews, textenews FROM news');
+?>
+
+<html>
+    <head>
+        <title>page d'accueil</title>
+        <meta charset="UTF-8" >
+        <link rel="stylesheet" href="accueil.css">
+    </head>
+
+    <header>
+            <nav>
+                <ul id="navigation">
+                    <li><a href="ajoutArticle.php" title="Ecrire un nouvel article">Ajouter un article</a></li>
+                    <li><a href="inscription.php" title="Créer un compte">S'inscrire</a></li>
+                    <li><a href="connection_redacteur.php" title="Se connecter">Connection</a></li>
+                    <li><a href="traitement_deconnexion" title="Se déconnecter">Déconnection</a></li>
+                </ul>
+            </nav>
+    </header>
+
+    <body id="hautdepage">  
+        <h1 class="titre">Bienvenue sur notre site d'actualité!</h1> 
+        <div class="texte">
+            <p>Vous trouverez sur ce site des articles traitant de différentes actualités. <br> 
+            Il est possible de se créer un compte pour écrire un nouvel article. <br>
+            Voici ci-dessous quelques articles </p>
+            <p>Trier les articles par: </p>
+            <input type=text list=tri id="tri" >
+            <datalist id=tri>
+                <option> Date
+                <option> Thème
+            </datalist>
+        </div>
+
+        <!--affichage des articles et de leur titre -->
+        <?php
+            foreach($result as $row){
+                echo utf8_encode("<h1 class='titre'>".$row['titrenews']."</h1><br>" );
+                echo utf8_encode("<p class='article'>".$row['textenews']."</p>");
+            }
+        ?>
+
+    </body>
+
+    <footer>
+        <a href="#hautdepage" style="margin-left: 20px;"> retourner en haut de la page </a> 
+        <a href="https://github.com/maximejournet54/projet_PHP">Lien du code du projet</a>
+        <p>Ce site a été développé par Lucas LEVIS et Maxime Journet dans le cadre du projet PHP de 2e année de DUT informatique.</p>           
+    </footer>
+</html>
--- ajoutArticle.php
+++ ajoutArticle.php
@@ -1,14 +1,11 @@
 <?php
     include "connection_bdd.php";
     error_reporting(E_ALL & ~E_NOTICE);
-    $req='SELECT description from THEME';
-    $result=$objPdo->prepare($req);
-    $result->execute();
+    $result = $objPdo->query('SELECT description FROM theme ORDER BY description ASC');
    
         if(isset($_POST['ajouter'])){
             $erreur=[];
             $value=[];
-
             if (!isset($_POST['theme'])) {
                 $erreur['theme']='selection obligatoire du theme';
             }
@@ -29,14 +26,13 @@
                 $value['texte']=trim($_POST['texte']);
             }
 
-
             if (count($erreur)==0) {
                 include "connection_bdd.php";
                 $req2="INSERT INTO news(idnews,idtheme,titrenews,datenews,textenews,idredacteur) VALUES (:idnews,:idtheme,:titrenews,:datenews,:textenews,:idredacteur)";
                 $insert=$objPdo->prepare($req2);
 
                 $idnews=rand(6,100);//generation d'un nb aleatoire
-                $date=date("y.m.d");//generation de la date du jour au format AAAA/MM/JJ comme dans la bdd
+                $date=date("y/m/d/h/i/s");//generation de la date du jour au format AAAA/MM/JJ/h/m/s  comme dans la bdd
                 //$idredacteur=
                 $insert->execute(array('idnews'=>$idnews,':idtheme'=>$_POST['theme'],':titrenews'=>$_POST['titre'], 'datenews'=>$date,
                 ':textenews'=>$_POST['texte'], ':idredacteur'=>$idredacteur));
@@ -58,7 +54,7 @@
         <title>Ajout d'un article</title>
     </head>
     <body>
-        <center>
+        <center> <!-- balises center a enlever et utiliser css-->
             <header>
                 <nav>
                     <ul>
@@ -66,46 +62,22 @@
                             <a href="./accueil.html">
                                 <div>Retour à l'accueil</div>
                             </a>
-                            <div>Retour à l'accueil</div>
-                        </a>
                         </li>
                     </ul>
                 </nav>
             </header>
         
             <h1 style="color: white;">Ajout d'un article</h1>
-
             <form method="POST" name="ajoutArticle" style="color: violet;">
                 <label for="theme">theme :</label>
-                <input type="radio" name="theme" id="culture"  value="culture">
-                <label for="culture">culture</label>
-                <input type="radio" name="theme" id="enquête"  value="enquête">
-                <label for="enquête">enquête</label> 
-                <input type="radio" name="theme" id="environnement"  value="environnement">
-                <label for="environnement">environnement</label>
-                <br>
-                <input type="radio" name="theme" id="fait_divers"  value="fait divers">
-                <label for="fait_divers">fait divers</label>
-                <input type="radio" name="theme" id="insolite"  value="insolite">
-                <label for="insolite">insolite</label>
-                <input type="radio" name="theme" id="international"  value="international">
-                <label for="international">international</label>
-                <br>
-                <input type="radio" name="theme" id="monde"  value="monde">
-                <label for="monde">monde</label>
-                <input type="radio" name="theme" id="people"  value="people">
-                <label for="people">people</label>
-                <input type="radio" name="theme" id="politique"  value="politique">
-                <label for="politique">politique</label>
-                <input type="radio" name="theme" id="santé"  value="santé">
-                <label for="santé">santé</label>
-                <br>
-                <input type="radio" name="theme" id="sciences"  value="sciences">
-                <label for="sciences">sciences</label>
-                <input type="radio" name="theme" id="société"  value="société">
-                <label for="société">société</label>
-                <input type="radio" name="theme"  id="sport" value="sport">
-                <label for="sport">sport</label>
+                <input type="text" list=theme>
+                <datalist id=theme>
+                    <?php
+                        foreach ($result as $value) {
+                            echo "<option>".$value['description'];
+                        }
+                    ?>
+                </datalist>
                 <br>
                 <br>
                 <label for="titre">titre : </label><input type="text" name="titre" placeholder="insérer le titre" value="<?php echo $value['titre']?>" required>
@@ -117,7 +89,7 @@
                 <label for="texte">texte : </label> <textarea name="texte" placeholder="insérer le texte" maxlength=500 value="<?php echo $value['texte']?>" required></textarea>
                 <br>
                 <span class="erreur"><?php echo $erreur['texte']?></span> 
-                <br>
+                <br>       
                 <input type="submit" name="valider" value="valider">
             </form>
         </center>
